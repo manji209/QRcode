7f1a2d22cc1207173e758d70e6202220ba31ddc3
Dinh Ho <daniel25th@gmail.com>
2018-12-27 20:21:22 -0800

    from laptop

--- load_data.py
+++ load_data.py
@@ -8,20 +8,21 @@ file = 'inventory_list.xlsx'
 # Load spreadsheet
 df = pd.ExcelFile(file)
 
+# Parse the Excel sheet into a pandas dataframe
 df1 = df.parse('Inventory List Items')
-#df = pd.DataFrame(columns=['ITEM ID', 'DESCRIPTION', 'IN STOCK'])
+
+# Make/Open a connection to the database
 conn = sqlite3.connect('product.db')
+
 cur = conn.cursor()
+
+# Iterate through the dataframe by row and insert data into the database
 for index, row in df1.iterrows():
     cur.execute('''INSERT INTO ProductInfo (ItemID, Description, InStock)
     VALUES (?,?,?)''', (row['SKU'], row['DESC-1'], row['IN STOCK']))
-    #print(row['SKU'], row['DESC-1'], row['IN STOCK'])
 
 
+# Commit or write changes to the database
 conn.commit()
 
-'''
 
-conn = sqlite3.connect('product.db')
-cur = conn.cursor()
-'''
